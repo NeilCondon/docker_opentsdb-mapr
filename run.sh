7d0fc61d84859bc174949a5e2d9070c5351f752c
Neil C <neil.condon@gmail.com>
2018-10-08 01:34:09 +0100

    Export all vars so they are in env list for later; fixed replace function so its okay with / in paths.

--- run.sh
+++ run.sh
@@ -7,18 +7,18 @@
 #
 
 # Some preliminary variables.   Some will have been set in the Dockerfile.
-MAPR_HOME=${MAPR_HOME:-/opt/mapr}
-OTSDB_HOME="${OTSDB_HOME:-/opt/mapr/opentsdb/opentsdb-$(</opt/mapr/opentsdb/opentsdbversion)}"
-OTSDB_CONF_FILE="${OTSDB_HOME}/etc/opentsdb/opentsdb.conf"
-MAPR_ZK_HOSTS="${MAPR_ZK_HOSTS:-$(maprcli node listzookeepers)}"
-JVMARGS="-enableassertions -enablesystemassertions -Xms$JVM_START_MEM -Xmx$JVM_MAX_MEM"
+export MAPR_HOME=${MAPR_HOME:-/opt/mapr}
+export OTSDB_HOME="${OTSDB_HOME:-/opt/mapr/opentsdb/opentsdb-$(</opt/mapr/opentsdb/opentsdbversion)}"
+export OTSDB_CONF_FILE="${OTSDB_HOME}/etc/opentsdb/opentsdb.conf"
+export MAPR_ZK_HOSTS="${MAPR_ZK_HOSTS:-$(maprcli node listzookeepers)}"
+export JVMARGS="-enableassertions -enablesystemassertions -Xms$JVM_START_MEM -Xmx$JVM_MAX_MEM"
 
 # the MAPR_CONTAINER_USER and MAPR_CLUSTER are only known at runtime, so the table paths cannot be set until runtime
-TSDB_TABLES_ROOT=/mapr/${MAPR_CLUSTER}/user/${MAPR_CONTAINER_USER}/opentsdb-default \
-OT_TSD_o_STORAGE_o_HBASE_o_DATA_TABLE=${TSDB_TABLES_ROOT}/tsdb \
-OT_TSD_o_STORAGE_o_HBASE_o_META_TABLE=${TSDB_TABLES_ROOT}/tsdb-meta \
-OT_TSD_o_STORAGE_o_HBASE_o_UID_TABLE=${TSDB_TABLES_ROOT}/tsdb-uid \
-OT_TSD_o_STORAGE_o_HBASE_o_TREE_TABLE=${TSDB_TABLES_ROOT}/tsdb-tree
+export TSDB_TABLES_ROOT=/mapr/${MAPR_CLUSTER}/user/${MAPR_CONTAINER_USER}/opentsdb-default \
+export OT_TSD_o_STORAGE_o_HBASE_o_DATA_TABLE=${TSDB_TABLES_ROOT}/tsdb \
+export OT_TSD_o_STORAGE_o_HBASE_o_META_TABLE=${TSDB_TABLES_ROOT}/tsdb-meta \
+export OT_TSD_o_STORAGE_o_HBASE_o_UID_TABLE=${TSDB_TABLES_ROOT}/tsdb-uid \
+export OT_TSD_o_STORAGE_o_HBASE_o_TREE_TABLE=${TSDB_TABLES_ROOT}/tsdb-tree
 
 
 #####################################################################################
@@ -32,10 +32,11 @@ function replace_or_add_configOption () {
     local after=$4
 
     # Replace using sed (this will fail if the config isn't present)
-    sed -i "s/.*$config.*/$config = $value/g" $file
+    echo "Setting $config = $value in $file."
+    sed -i "s|.*$config.*|$config = $value|g" $file
 
     # grep for the config.   If it's not found, add it after the first line containing the matching string.
-    grep -q "$config" $file || sed -i "0,/^$after\+/s//\0\n$config = $value/" $file
+    # grep -q "$config" $file || sed -i "0,/^$after\+/s//\0\n$config = $value/" $file
 }
 
 
