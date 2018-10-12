f864b3e860961373a6712743b5706e1325902d76
Neil Condon <ncondon@gtc3.eddisc-prod.edwardshighvacuum.net>
2018-10-12 19:00:17 +0100

    Fixed several issues with debug messages, as well as appending the ticketfile location to 'docker_args'.   Also had to strip first lines from maprcli output for looking up CLDBs and Zookeepers.

--- start_opentsdb-mapr.sh
+++ start_opentsdb-mapr.sh
@@ -8,20 +8,20 @@ read -s -p 'Enter password: ' passw
 echo \
 
 # Get the cluster name
-if [ -f $MAPR_HOME/conf/mapr-clusters/conf ]; then
+if [ -f $MAPR_HOME/conf/mapr-clusters.conf ]; then
     # get the first cluster name listed in the mapr-clusters.conf
     cluster=$(head -1 /opt/mapr/conf/mapr-clusters.conf | cut -f 1 -d " ")
-    echo 'We appear to be running on a node of cluster: $cluster'
+    echo 'We appear to be running on a node of cluster:' $cluster
 else
     read -p 'Cluster name: ' cluster
 fi
 
 # Get the list of CLDB and Zookeeper nodes
 if [ -f $MAPR_HOME/bin/maprcli ]; then
-    cldbs=$(maprcli node listcldbs)
-    echo 'Discovered these CLDBs for cluster $cluster:  $cldbs'
-    zkeepers=$(maprcli node listzookeepers)
-    echo 'Zookeeper hosts:  $zkeepers'
+    cldbs=$(maprcli node listcldbs | sed 1d)
+    echo 'Discovered these CLDBs for cluster' $cluster':' $cldbs
+    zkeepers=$(maprcli node listzookeepers | sed 1d)
+    echo 'Zookeeper hosts:' $zkeepers
 else
     read -p 'List of CLDBs: ' cldbs
     read -p 'List of Zookeeper nodes: ' zkeepers
@@ -45,7 +45,7 @@ docker_args="-p 4242:$otsdb_port \
 # Refresh the MapR service ticket
 if [ -f $MAPR_HOME/bin/maprlogin ]; then
     echo 'Generating service ticket...'
-    ticket_path=/tmp/maprserviceticket_dsr_$user
+    ticket_path=/tmp/maprserviceticket_otsdb_$user
     if [ -f $ticket_path ]; then
         sudo rm -rf $ticket_path
     fi
@@ -56,9 +56,7 @@ if [ -f $MAPR_HOME/bin/maprlogin ]; then
         -duration 365:0:0 \
         -renewal 730:0:0
     sudo chown $user:$user $ticket_path
-    docker_args=$docker_args \
-		  -e MAPR_TICKETFILE_LOCATION=/tmp/maprticket \
-                  -v $ticket_path:/tmp/maprticket:ro
+    docker_args=$docker_args '-e MAPR_TICKETFILE_LOCATION=/tmp/maprticket -v' $ticket_path':/tmp/maprticket:ro'
 else
     echo "The 'maprlogin' command is not availabe on this host; a MapR ticket could not be created."
     echo 'Defaulting to non-secure mode.'
